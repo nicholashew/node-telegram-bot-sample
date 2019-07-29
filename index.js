fa69cccc9b8d7c1f6de695b67df1d3594a8e7329
Nicholas <nicholashew@users.noreply.github.com>
2019-07-29 17:40:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 const path = require('path');
@@ -10,37 +9,67 @@ const config = require('./config/config.json');
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(config.telegram_bot.token, { polling: true });
 
+// sample document directory path
 const documentDirectoryPath = path.join(__dirname, 'document');
 
 // Listen to all message
 bot.on('message', (msg) => {
   console.log(`Received message: ${msg.text}`);
   const chatId = msg.chat.id;
+
+  if (/\/resource (.+)/.test(msg.text) || /\/ping($)/.test(msg.text)) {
+    console.log('Ignore for command messages');
+    return;
+  }
+
   bot.sendMessage(chatId, 'Received message: ' + msg.text);
 });
 
 // Listen to message matches "/ping" exact string
-bot.onText(/\/ping($)/, (msg, match) => {
+bot.onText(/\/ping($)/, (msg) => {
   console.log(`Received command /ping`);
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, 'pong');
 });
 
-// Listen to message matches "/report {reportName}" 
-bot.onText(/\/report (.+)/, async (msg, match) => {
-  console.log(`Received command /report {reportName}`);
+// Listen to message matches "/resource {resource_type}" 
+bot.onText(/\/resource (.+)/, async (msg, match) => {
+  console.log(`Received command /resource {resource_type}`);
   const chatId = msg.chat.id;
   const senderName = msg.from.first_name;
-  const reportName = match[1]; // the captured "reportName"
+  const resourceType = match[1]; // the captured "resource_type"
+  const filename = 'resource-' + resourceType + '-' + new Date().toISOString();
   const greet = getTimeGreeting();
-  bot.sendMessage(chatId, `${greet} ${senderName}, attached report ${reportName} as requested.`);
+  bot.sendMessage(chatId, `${greet} ${senderName}, attached resource ${resourceType} as requested.`);
 
-  const buffer = await fs.readFileSync(documentDirectoryPath + '/sample.txt');
-  const fileOptions = {    
-    filename: 'report-' + reportName + '-' + new Date().toISOString() + '.txt',
-    contentType: 'application/octet-stream',
-  };
-  bot.sendDocument(chatId, buffer, {}, fileOptions);
+  switch (resourceType) {
+    case 'audio': {
+      bot.sendAudio(chatId, 'https://sample-videos.com/audio/mp3/crowd-cheering.mp3');
+      break;
+    }
+    case 'image': {
+      const buffer = await fs.readFileSync(documentDirectoryPath + '/sample.jpg');
+      bot.sendPhoto(chatId, buffer, { caption: filename });
+      break;
+    }
+    case 'video': {
+      bot.sendVideo(chatId, 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4');
+      break;
+    }
+    case 'text': {
+      const textBuffer = await fs.readFileSync(documentDirectoryPath + '/sample.txt');
+      const fileOptions = {
+        filename: filename + '.txt',
+        contentType: 'application/octet-stream',
+      };
+      bot.sendDocument(chatId, textBuffer, {}, fileOptions);
+      break;
+    }
+    default: {
+      bot.sendMessage(chatId, `Invalid request, resource ${resourceType} is not available.`);
+      break;
+    }
+  }
 });
 
 const getTimeGreeting = () => {
